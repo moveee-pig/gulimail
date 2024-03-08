e198c57543b42982058295f6e4f53ff1663d4b10
yyb <1574674418@qq.com>
2024-03-08 15:42:06 +0800

    项目初始结构创建

---
+++ gulimall-ware/src/test/java/com/yyb/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyb.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
